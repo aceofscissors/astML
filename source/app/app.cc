25662b5b066f982548a7f7713f3be98374a02eba
Lavínia Rodrigues <xenia@kernel32.xyz>
2024-08-30 00:32:43 -0300

    feat: parsing source files from CLI arguments

--- source/app/app.cc
+++ source/app/app.cc
@@ -1,11 +1,33 @@
 #include <astML/app.hh>
 #include <astML/util.hh>
 
-#include <print>
+#include <astML/astML.hh>
 
+#include <array>
+#include <format>
+#include <print>
+#include <ranges>
 #include <stdexcept>
+#include <string>
+#include <string_view>
 
 namespace astML {
+
+namespace {
+
+auto split_string(const std::string_view &str,
+                  const std::string_view &delim) -> std::array<std::string> {
+  std::vector result;
+
+  const auto split = std::views::split(str, delim);
+  for (const auto &substr : split) {
+    result.push_back({substr});
+  }
+
+  return result;
+}
+
+} // namespace
 
 application::application(const program_arguments &&args)
     : launch_arguments{std::move(args)} {
@@ -13,8 +35,21 @@ application::application(const program_arguments &&args)
 }
 
 auto application::run() -> void {
-  std::println("funfa!");
-  return;
+  auto astml = std::unique_ptr<astmlib::runner>();
+
+  constexpr bool const verbose =
+      true; // TODO(Lavinia): turn this into a CLI option
+
+  for (const auto &path_arg : this->launch_arguments) {
+    const auto &[filename, _] = split_string(path_arg, ".");
+
+    auto xml = astml->parse_from(path_arg);
+    xml.write(std::format("{}.{}", filename, "xml"));
+
+    if (verbose) {
+      std::println("parsed {} successfully", filename);
+    }
+  }
 }
 
 } // namespace astML
